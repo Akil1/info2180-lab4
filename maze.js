157fd43b5e4ddcb44da9bef5fb35f59da00befbb
Akil Nassor <akil94nassor@gmail.com>
2016-10-14 13:10:19 -0500

    Add exercise 1
    Single boundary turns red

--- maze.js
+++ maze.js
@@ -1,2 +1,7 @@
-"use strict";
+window.onload = function() {
+    document.getElementById("boundary1").onmouseover = overBoundary;
+}
 
+function overBoundary() {
+    document.getElementById("boundary1").setAttribute("class","boundary youlose");
+}
