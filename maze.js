258fc59e8f2db1faf914945bfa8b1f4cbe4210fd
Akil Nassor <akil94nassor@gmail.com>
2016-10-14 20:18:40 -0500

    Add exercise 2
    All boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -1,7 +1,23 @@
-window.onload = function() {
+/*window.onload = function() {
     document.getElementById("boundary1").onmouseover = overBoundary;
 }
 
 function overBoundary() {
     document.getElementById("boundary1").setAttribute("class","boundary youlose");
+}
+
+*/
+
+window.onload = function() {
+    var numBoundaries1 = document.querySelectorAll("#maze .boundary");
+    for (var num = 0; num < numBoundaries1.length; num++) 
+        numBoundaries1[num].onmouseover = myfunction;
+    
+}
+
+function myfunction() {
+    var numBoundaries2 = document.querySelectorAll("#maze .boundary");
+    for (var num = 0; num < numBoundaries2.length; num++) 
+        numBoundaries2[num].className = "boundary youlose";
+    
 }
